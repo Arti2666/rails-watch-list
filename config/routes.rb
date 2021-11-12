6818cc3df130c65dc72276f7bf537f862360d9b8
Arthur Perissich <arthurp@alumni.ie.edu>
2021-11-12 16:37:18 +0100

    Working Beta

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get "/movies", to: 'movies#index'
-  resources :lists, only: [:index, :create, :show, :new ] do
+  delete "/list/:id", to: 'lists#delete', as: 'listdelete'
+  resources :lists, only: [:index, :create, :show, :new,] do
     resources :bookmarks, only: [:index, :new, :create]
   end
 
